b0ca17976edb9bc1291c5802ac896cdd4c8fa168
7piyushdev <dchlamydia@gmail.com>
2022-11-23 21:33:37 +0530

    dom and index files

---
+++ dom.js
@@ -0,0 +1,27 @@
+// Examining the DOCUMENT OBJECT
+
+console.dir(document.domain)
+console.dir(document.URL)
+console.dir(document.title)
+
+console.dir(document.doctype)
+console.dir(document.head)
+console.dir(document.all) // gives an array, an html collection prent inside the DOM. Can access the elements with indexes.
+
+console.log(document.all[10]); // gives the h1 element
+console.log(document.images); // gives an empt array as we dont have any images
+
+// document.all[10].textContent = "hello me" not the best practice
+
+var headingTitle = document.getElementById("header-title");
+console.log(headingTitle);
+// headingTitle.textContent = 'Hello TC'
+// headingTitle.innerText = 'Hi IT'
+// console.log(headerTitle.innerText)
+
+// innerText overwrote the tectContent text because innerText pays attention to the styling. In span it shows the text in both title and console. but With textContent the span text will disappear from screen but shows in console.
+
+headingTitle.innerHTML = '<h3>Hello</h3>'
+headingTitle.style.border = '2px solid black'
+
+
